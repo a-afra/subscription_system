701f8ef59f290fac238b0ebc70355b229b9c4e88
Afra <afraaalireza@gmail.com>
2023-04-20 17:49:08 +0330

    Add secret key!

--- src/subscriptionproject/subscriptionproject/settings.py
+++ src/subscriptionproject/subscriptionproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-j6$u*1#16urhble7vor)c)1t-a_f#hk_13e)cpde2y4!4gow28'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
